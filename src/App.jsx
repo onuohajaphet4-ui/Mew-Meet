8910d4a3831338b018b1a7c4bc8427bdf7700860
Juice <chibuoyimk@gmail.com>
2026-09-23 16:25:01 +0100

    secom commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {  Routes, Route } from "react-router-dom";
-import MainLayout from "./layouts/MainLayout.jsx";
+import MainLayout from "./Layouts/MainLayout.jsx";
 import './App.css'
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
